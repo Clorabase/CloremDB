9cefbe234e982e4903961ddd5863f59fffc7c00b
Rahil khan <65817230+ErrorxCode@users.noreply.github.com>
2021-10-14 11:22:10 +0530

    initial commit

---
+++ CloremDB/src/main/java/com/clorem/db/CloremDatabaseException.java
@@ -0,0 +1,15 @@
+package com.clorem.db;
+
+public class CloremDatabaseException extends RuntimeException {
+    Reasons reason;
+
+    public CloremDatabaseException(String message,Reasons reason){
+        super(message);
+        this.reason = reason;
+    }
+
+
+    public Reasons getReason() {
+        return reason;
+    }
+}
